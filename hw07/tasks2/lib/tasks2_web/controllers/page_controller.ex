38363e7e5c61a5556486148e48009247919f2a7e
Vipul Sharma <vipulsharma018@gmail.com>
2018-02-26 15:33:09 -0500

    Manager added

--- hw07/tasks2/lib/tasks2_web/controllers/page_controller.ex
+++ hw07/tasks2/lib/tasks2_web/controllers/page_controller.ex
@@ -4,4 +4,10 @@ defmodule Tasks2Web.PageController do
   def index(conn, _params) do
     render conn, "index.html"
   end
+
+  def feed(conn, _params) do
+  tasks = Tasks2.UserTasks.list_tasks()
+  changeset = Tasks2.UserTasks.change_task(%Tasks2.UserTasks.Task{})
+  render conn, "feed.html", tasks: tasks, changeset: changeset
+end
 end
